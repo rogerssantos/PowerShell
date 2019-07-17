ad1c4869c016a7c2c40152ff4008197415a4260d
Roger Santos <roger_santos@outlook.com>
2019-07-17 11:16:42 -0300

    Update Unifies text files.ps1

--- Commands/Unifies text files.ps1
+++ Commands/Unifies text files.ps1
@@ -1,2 +1,11 @@
 ## Get all files started by text and unifies into a new files called all.txt
 Get-Content text* | Set-Content all.txt
+
+## Get content from multiple files and remove duplicated and blank lines
+$hash = @{}
+$FileName = "unified_files.txt"
+if (Test-Path $FileName) 
+{
+  Remove-Item $FileName
+}
+Get-Content *.txt | ? {$_.trim() -ne "" } | %{if($hash.$_ -eq $null) { $_ }; $hash.$_ = 1} > $FileName
